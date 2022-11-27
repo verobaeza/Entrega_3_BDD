a0ae03aed112b8d0c6e992e4562e3717e7c49fbb
VERONICA BAEZA <vbaezac@uc.cl>
2022-11-27 11:34:02 -0300

    importar usuarios

--- queries/importar_usuarios.php
+++ queries/importar_usuarios.php
@@ -4,13 +4,13 @@
     require("../config/conexion.php");
 
     
-    $query = "SELECT * FROM artistas ;"; # Obtenemos todos los artistas
+    $query = "SELECT * FROM artistas;"; # Obtenemos todos los artistas
     $result = $db1 -> prepare($query); # Se conecta a la BDD impar
     $result -> execute();
     $artistas = $result -> fetchAll();
 
     foreach ($artistas as $artista){
-        $query = "SELECT importar_usuarios ($artista[0], '$artista[1]'::varchar, 'artista'::varchar);";
+        $query = "SELECT importar_usuarios ($artista[0], '$artista[1]'::varchar, artista::varchar);";
         // Ejecutamos las querys para efectivamente insertar los datos
         
         $result = $db1 -> prepare($query);
