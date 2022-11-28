6edec84e9ef81f5064e114baf1796bac221e4b44
VERONICA BAEZA <vbaezac@uc.cl>
2022-11-28 13:21:59 -0300

    cambio importar usuarios

--- queries/importar_usuarios.php
+++ queries/importar_usuarios.php
@@ -13,7 +13,7 @@
     foreach ($artistas as $artista){
         $query = "SELECT importar_usuarios ($artista[0], '$artista[1]'::varchar, '$tipo'::varchar);";
         echo gettype($artista[0]) . "<br>";
-        echo gettype($artista[1]::varchar) . "<br>";
+        echo gettype($artista[1]) . "<br>";
         echo gettype($tipo) . "<br>";
         // Ejecutamos las querys para efectivamente insertar los datos
         
