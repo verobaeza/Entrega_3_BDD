013e89fef58b02eb33bc669a800fab4b2194f435
VERONICA BAEZA <vbaezac@uc.cl>
2022-11-28 20:39:30 -0300

    form iniciar sesion

--- index.php
+++ index.php
@@ -7,8 +7,18 @@
     <form align="center" action="./queries/importar_usuarios.php" method="GET">
         <input class='btn' type="submit" value="Importar">
     </form>
-
     <br>
-
+    <h3 align="center"> Iniciar sesión </h3>
+    <form  align="center" action='./queries/iniciar_sesion.php' method='POST'>
+    <div class='form-element'>
+        <label for='name'>Nombre usuario</label>
+        <input type='text' name='nombre_usuario' />
+    </div>
+    <div class='form-element'>
+        <label for='name'>Clave</label>
+        <input type='text' name='clave' />
+    </div>
+    <div class='form-element'>
+        <input class='btn' type="submit" value="Ingresar">
 </body>
 </html>
