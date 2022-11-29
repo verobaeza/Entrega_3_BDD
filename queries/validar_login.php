884c1e721a79a36a452c99d4bb3a8da0372592b5
VERONICA BAEZA <vbaezac@uc.cl>
2022-11-28 23:13:02 -0300

    verificar login

--- queries/validar_login.php
+++ queries/validar_login.php
@@ -20,17 +20,19 @@
         $result -> execute();
         $usuarios = $result -> fetchAll();
 
-        echo "$usuarios";
-
-        if (!empty($usuario)){
-            echo "$usuario[3]";
-            #if ($usuario[3] = 'artista'){
-            #    echo "pagina_artista.php";
-            #}
-            #elseif ($usuario[3] = 'productora'){
-            #    echo "pagina_productora.php";
-            #}
+        foreach($usuarios as $usuario){
+            if (!empty($usuario)){
+                echo "$usuario[3]";
+                #if ($usuario[3] = 'artista'){
+                #    echo "pagina_artista.php";
+                #}
+                #elseif ($usuario[3] = 'productora'){
+                #    echo "pagina_productora.php";
+                #}
+            }
         }
+
+
 
         #$msg = "Sesión iniciada correctamente";
         #header("Location: ../index.php?msg=$msg");
