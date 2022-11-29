84cb0eb8a7e23b46eb28b54d2165f4f24c959ab0
VERONICA BAEZA <vbaezac@uc.cl>
2022-11-28 23:21:17 -0300

    verificar login

--- queries/validar_login.php
+++ queries/validar_login.php
@@ -14,7 +14,7 @@
         $_SESSION['nombre_ingresado'] = $_POST['nombre_ingresado'];
         $_SESSION['clave_ingresada'] = $_POST['clave_ingresada'];
 
-        $query = "SELECT * FROM usuarios WHERE nombre_usuario = $nombre_ingresado AND clave = $clave_ingresada;"; 
+        $query = "SELECT * FROM usuarios WHERE nombre_usuario = '$nombre_ingresado' AND clave = '$clave_ingresada';"; 
         # echo " $nombre_ingresado, $clave_ingresada";
         $result = $db1 -> prepare($query); # Enviamos la consulta a la BDD impar
         $result -> execute();
