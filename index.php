97b48f061de5ce3023ba8bf56fb36581b737a2c5
VERONICA BAEZA <vbaezac@uc.cl>
2022-11-29 12:24:17 -0300

    style y template

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php include('./templates/header.html');?>
     <body>
-        <h1 align="center"> Entrega 3 - Bases de Datos </h1>
-        <h3 align="center"> Grupos 107 y 116 </h3>
-        <br>
-        <h3 align="center"> Importar usuarios </h3>
-        <form align="center" action="./queries/importar_usuarios.php" method="GET">
-            <input class='btn' type="submit" value="Importar">
-        </form>
-        <br>
-        <h3 align="center"> Iniciar sesión </h3>
-        <br>
-        <form align="center" class="form-signin" role="form" action="./queries/validar_login.php" method="post">
-            <input type="text" name="nombre_ingresado" placeholder="Nombre usuario" required autofocus>
-            <input type="password" name="clave_ingresada" placeholder="Clave" required>
-            <button type="submit" name="login"> Ingresar </button>
-        </form>
+        <div class='main'>
+            <h1 class='title'>Entrega 3 - Bases de Datos </h1>
+            <h3 class='subtitle'> Grupos 107 y 116 </h3>
+        <div class='container'>
+            <h3>Importar usuarios </h3>
+            <form action="./queries/importar_usuarios.php" method="GET">
+                <input class='btn' type="submit" value="Importar">
+            </form>
+        </div>
+        <div class='container'>
+            <h3> Iniciar sesión </h3>
+            <form class="form-signin" role="form" action="./queries/validar_login.php" method="post">
+                <input type="text" name="nombre_ingresado" placeholder="Nombre usuario" required autofocus>
+                <input type="password" name="clave_ingresada" placeholder="Clave" required>
+                <button type="submit" name="login"> Ingresar </button>
+            </form>
+        </div>
     </body>
 </html>
