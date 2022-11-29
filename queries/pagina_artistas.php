776709180a68b7e07a5c8b27bbf536723e7df5a4
VERONICA BAEZA <vbaezac@uc.cl>
2022-11-29 16:42:23 -0300

    query tours

--- queries/pagina_artistas.php
+++ queries/pagina_artistas.php
@@ -19,7 +19,14 @@ session_start();
     $query = "SELECT evento, recinto, fecha_inicio FROM eventos WHERE aid = (SELECT ref_id FROM usuarios WHERE nombre_usuario = '$usuario'::varchar);"; 
     $result = $db1 -> prepare($query); # Nos conectamos a a la BDD impar
     $result -> execute();
-    $eventos = $result -> fetchAll();
+    $eventos1 = $result -> fetchAll(); # PRIMER RESULTADO
+
+    // OTROS ARTISTAS QUE PARTICIPAN EN EL EVENTO -------------------------------------------------------------
+    // TOUR AL QUE PERTENECE  ---------------------------------------------------------------------------------
+    $query = "SELECT nombre FROM tours WHERE nombre = (SELECT evento FROM eventos WHERE aid = (SELECT ref_id FROM usuarios WHERE nombre_usuario = '$usuario'::varchar);)"
+    $result = $db1 -> prepare($query); # Nos conectamos a a la BDD impar
+    $result -> execute();
+    $eventos2 = $result -> fetchAll(); # SEGUNDO RESULTADO
 
 ?>
     <body>  
@@ -29,15 +36,17 @@ session_start();
                 <th>Evento</th>
                 <th>Recinto</th>
                 <th>Fecha inicio</th>
+                <th>Tour</th>
                 <tr>
             </thead>
             <tbody>
                 <?php
-                foreach ($eventos as $evento) {
+                foreach ($eventos1 as $evento) {
                     echo "<tr>";
                     echo "<td>$evento[0]</td>";
                     echo "<td>$evento[1]</td>";
                     echo "<td>$evento[2]</td>";
+                    foreach($eventos2 as $tour){echo "<td>$tour[2]</td>"};
                     echo "</tr>";
                 }
                 ?>
