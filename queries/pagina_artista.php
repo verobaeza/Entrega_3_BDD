4a5e49d47325f7bca1e2825ef88d41857f881916
VERONICA BAEZA <vbaezac@uc.cl>
2022-11-29 11:59:00 -0300

    style y template

--- queries/pagina_artista.php
+++ queries/pagina_artista.php
@@ -1,4 +1,4 @@
-<?php include('./templates/header.html');   ?>
+<?php include('../templates/header.html');   ?>
 <body>
     <h1 align="center"> Bienvenid@ a la página para artistas! </h1>
 </body>
--- queries/pagina_productora.php
+++ queries/pagina_productora.php
@@ -1,4 +1,4 @@
-<?php include('./templates/header.html');   ?>
+<?php include('../templates/header.html');   ?>
 <body>
     <h1 align="center"> Bienvenid@ a la página para productoras! </h1>
 </body>
